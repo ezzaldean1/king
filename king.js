ca65c6ff1307f6c89ed4c163a90a1f41995b6d5f
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-30 23:57:32 +0300

    Update king.js

--- king.js
+++ king.js
@@ -13,11 +13,11 @@ client.on('ready', () => {
   console.log(' Mob2 Bot Is Online')
   console.log('---------------')
   client.user.setStatus("Online")
-    client.user.setActivity(" Type ihelp",{type: 'WATCHING'})
+    client.user.setActivity(" Type yhelp",{type: 'WATCHING'})
 	
 });
 	
-            var prefix = "i" ;
+            var prefix = "y" ;
 	
 	
 	/*
@@ -31,7 +31,7 @@ npm install queue
 */
 client.on('message', msg => {
 
-    if (msg.content == 'iplay') {
+    if (msg.content == 'yplay') {
         if (msg.member.voiceChannel) {
 
      if (msg.member.voiceChannel.joinable) {
@@ -232,8 +232,8 @@ function play(guild, song) {
 	serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
 
-const adminprefix = "tvip";//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
-const devs = ['454400689133715477'];//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+const adminprefix = "y";//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+const devs = ['470500980342128650'];//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
 client.on('message', message => {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
   var argresult = message.content.split(` `).slice(1).join(' ');//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
     if (!devs.includes(message.author.id)) return;//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
